a57f2cc2db3fe511e9fcb204b472f522f566584a
Pedro Arenas <pedroars99@icloud.com>
2022-05-21 18:00:07 +0200

    Svelte Crash Course: Add transitions

--- Frontend/Svelte/feedback-app/src/components/feedback/FeedbackList.svelte
+++ Frontend/Svelte/feedback-app/src/components/feedback/FeedbackList.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
     import type { Feedback } from "../../models/Feedback";
+    import { fade, scale } from "svelte/transition";
     import FeedbackForm from "./FeedbackForm.svelte";
     import FeedbackItem from "./FeedbackItem.svelte";
     import FeedbackStats from "./FeedbackStats.svelte";
 
     export let feedbackItems: Feedback[] = [];
 
-    const handleAdd = ({detail}) => {
+    const handleAdd = ({ detail }) => {
         feedbackItems = [...feedbackItems, detail];
     };
 
-    const handleDelete = ({detail }) => {
+    const handleDelete = ({ detail }) => {
         feedbackItems = feedbackItems.filter((item) => item.id !== detail);
     };
 </script>
 
 <!-- HTML Section -->
-<FeedbackForm on:add-feedback-item={handleAdd}/>
+<FeedbackForm on:add-feedback-item={handleAdd} />
 
 <FeedbackStats {feedbackItems} />
 
 {#each feedbackItems as fb}
-    <FeedbackItem feedback={fb} on:delete-feedback-item={handleDelete} />
+    <div in:scale out:fade={{ duration: 500 }}>
+        <FeedbackItem feedback={fb} on:delete-feedback-item={handleDelete} />
+    </div>
 {/each}
