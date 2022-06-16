787c660b8d38043849939319f5b87f0e16766ad1
Pedro Arenas <pedroars99@icloud.com>
2022-06-16 18:15:05 +0200

    [SvelteKit] Add custom navbart

--- Frontend/Svelte/SvelteKit/pokedex-app/src/components/layout/nav.svelte
+++ Frontend/Svelte/SvelteKit/pokedex-app/src/components/layout/nav.svelte
@@ -1,4 +1,43 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const navbarBurger = document.querySelector('.navbar-burger');
+		
+    navbarBurger?.addEventListener('click', () => {
+      navbarBurger.classList.toggle('is-active');
+      
+      const target = navbarBurger.getAttribute('data-target');
+      const targetElement = document.getElementById(target!);
+
+      targetElement?.classList.toggle('is-active');
+    })
+	});
+</script>
+
+<nav class="navbar" aria-label="main navigation">
+	<div class="navbar-brand">
+		<a class="navbar-item" href="/">
+			<img src="/pokeapi_256.png" alt="PokeApi Logo" />
+		</a>
+
+		<button
+			class="navbar-burger"
+			aria-label="menu"
+			aria-expanded="false"
+			data-target="navbarBasicExample"
+		>
+			<span class="is-active" aria-hidden="true" />
+			<span class="is-active" aria-hidden="true" />
+			<span class="is-active" aria-hidden="true" />
+		</button>
+	</div>
+
+	<div id="navbarBasicExample" class="navbar-menu">
+		<div class="navbar-start">
+			<a href="/" class="navbar-item"> Home </a>
+
+			<a href="/about" class="navbar-item"> About </a>
+		</div>
+	</div>
 </nav>
