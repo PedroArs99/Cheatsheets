9b347d9bb214e72b3f790f34d61f5ead89432f8b
Pedro Arenas <pedroars99@icloud.com>
2022-06-16 17:28:43 +0200

    [SvelteKit] Display pokemon names

--- Frontend/Svelte/SvelteKit/pokedex-app/src/routes/index.svelte
+++ Frontend/Svelte/SvelteKit/pokedex-app/src/routes/index.svelte
@@ -1,12 +1,20 @@
 <script>
+	import { fetchPokemon, pokemonStore } from '../stores/pokestore';
+
+	// Init Store Data
+	fetchPokemon();
 </script>
 
 <!-- HTML Section -->
 <svelte:head>
-    <title>Svelte Kit Pokedex</title>
+	<title>Svelte Kit Pokedex</title>
 </svelte:head>
 
 <h1>SvelteKit Pokedex</h1>
+
+{#each $pokemonStore as pokemon }
+	<h2>{pokemon.name}</h2>
+{/each}
 
 <!-- CSS Section-->
 <style>
