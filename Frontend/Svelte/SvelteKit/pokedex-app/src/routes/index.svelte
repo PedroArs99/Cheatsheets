e7e0a2621102ad42159f741429a671bdbc43a67a
Pedro Arenas <pedroars99@icloud.com>
2022-06-16 19:19:55 +0200

    [SvelteKit] Add filtering

--- Frontend/Svelte/SvelteKit/pokedex-app/src/routes/index.svelte
+++ Frontend/Svelte/SvelteKit/pokedex-app/src/routes/index.svelte
@@ -1,9 +1,23 @@
-<script>
+<script lang="ts">
 	import { fetchPokemon, pokemonStore } from '../stores/pokestore';
 	import PokemonCard from '../components/pokemon/pokemonCard.svelte';
+	import { onMount } from 'svelte';
+	import type { Pokemon } from 'src/models/pokemon';
 
-	// Init Store Data
-	fetchPokemon();
+	onMount(() => {
+		fetchPokemon();
+	});
+
+	let searchTerm = '';
+	let filteredPokemon = new Array<Pokemon>();
+
+	$: {
+		if(searchTerm){
+			filteredPokemon = $pokemonStore.filter(pokemon => pokemon.name.includes(searchTerm));
+		}else {
+			filteredPokemon = $pokemonStore;
+		}
+	}
 </script>
 
 <!-- HTML Section -->
@@ -11,15 +25,26 @@
 	<title>Svelte Kit Pokedex</title>
 </svelte:head>
 
-<div class="level">
-	<h1 class="level-item is-size-2 my-3">SvelteKit Pokedex</h1>
+<div class="container">
+	<div class="level">
+		<h1 class="level-item is-size-2 my-3">SvelteKit Pokedex</h1>
+	</div>
+
+	<div class="level">
+		<div class="level-item field px-3">
+			<div class="control">
+				<input class="input" type="text" placeholder="Search" bind:value={searchTerm} />
+			</div>
+		</div>
+	</div>
+
+	<div class="pokemon-list">
+		{#each filteredPokemon as pokemon}
+			<PokemonCard {pokemon} />
+		{/each}
+	</div>
 </div>
-<div class="pokemon-list">
-	{#each $pokemonStore as pokemon}
-		<PokemonCard {pokemon}/>
-	{/each}
-</div>
- 
+
 <!-- CSS Section-->
 <style>
 	.pokemon-list {
@@ -28,5 +53,9 @@
 		justify-content: space-around;
 
 		gap: 0.75em;
+	}
+
+	.control {
+		width: 100%;
 	}
 </style>
