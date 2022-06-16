d435237175d48d574181dfaef197288f8e8b569d
Pedro Arenas <pedroars99@icloud.com>
2022-06-16 18:57:19 +0200

    [SvelteKit] Add Pokemon Card

---
+++ Frontend/Svelte/SvelteKit/pokedex-app/src/components/pokemon/pokemonCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Pokemon } from 'src/models/pokemon';
+
+	export let pokemon: Pokemon;
+</script>
+
+<div class="card">
+	<div class="card-image has-text-centered">
+		<figure class="image is-128x128 is-inline-block">
+			<!-- svelte-ignore a11y-img-redundant-alt -->
+			<img src={pokemon.image} alt="Pokemon image" width="96" height="96" />
+		</figure>
+	</div>
+	<div class="card-content">
+		<div class="content">
+			<a href={`/pokemon/${pokemon.id}`} class="button is-ghost is-large is-fullwidth">{pokemon.id} - {pokemon.name}</a>
+		</div>
+	</div>
+</div>
+
+<style>
+	.card {
+		width: 250px;
+	}
+</style>
