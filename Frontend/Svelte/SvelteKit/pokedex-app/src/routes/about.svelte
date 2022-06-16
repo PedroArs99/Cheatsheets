2958f1f92e4d2419243f822f9713e7eae4715586
Pedro Arenas <pedroars99@icloud.com>
2022-06-16 16:49:50 +0200

    [SvelteKit] Add route and custom tab title

---
+++ Frontend/Svelte/SvelteKit/pokedex-app/src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About the Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This a really cool Svelte Kit Demo that uses the Pokemon API to build a demo</p>
--- Frontend/Svelte/SvelteKit/pokedex-app/src/routes/index.svelte
+++ Frontend/Svelte/SvelteKit/pokedex-app/src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<!-- HTML Section -->
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>SvelteKit Pokedex</h1>
+
+<!-- CSS Section-->
+<style>
+	h1 {
+		color: red;
+	}
+</style>
