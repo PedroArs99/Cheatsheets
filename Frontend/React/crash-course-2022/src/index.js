c47a1b0911612ea2358f7c9f93d40b2779dd159f
Pedro Arenas <pedroars99@icloud.com>
2022-07-17 13:34:52 +0200

    [React] Spread props

--- Frontend/React/crash-course-2022/src/index.js
+++ Frontend/React/crash-course-2022/src/index.js
@@ -5,12 +5,14 @@ import { createRoot } from "react-dom/client";
 import './index.css';
 
 const hackingK8s = {
+  key: 1,
   title: 'Hacking Kubernetes: Threat-Driven Analysis and Defense',
   author: 'Andrew Martin & Michael Hausenblas',
   img: "https://images-na.ssl-images-amazon.com/images/I/41keUsJJxPL._SX379_BO1,204,203,200_.jpg"
 }
 
 const cleanCode = {
+  key: 2,
   title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
   author: 'Robert C. Martin',
   img: 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg'
@@ -21,14 +23,12 @@ const books = [hackingK8s, cleanCode]
 function BookList() {
   return (
     <section className="bookList">
-      {books.map((book, index) => <Book key={index} book={book} />)}
+      {books.map((book) => <Book {...book} />)}
     </section>
   );
 }
 
-function Book({book}) {
-  const {title, author, img} = book
-
+function Book({title, author, img}) {
   return <article className="book">
     <img src={img}/>
     <h2>{title}</h2>
