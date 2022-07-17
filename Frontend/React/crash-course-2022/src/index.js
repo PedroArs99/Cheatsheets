3819b5852ed76ae1ce1ee29abfc741d3cb032ae9
Pedro Arenas <pedroars99@icloud.com>
2022-07-17 13:01:47 +0200

    [React] Use basic props

--- Frontend/React/crash-course-2022/src/index.js
+++ Frontend/React/crash-course-2022/src/index.js
@@ -4,34 +4,26 @@ import { createRoot } from "react-dom/client";
 // CSS
 import './index.css';
 
+const title = 'Hacking Kubernetes: Threat-Driven Analysis and Defense'
+const author = 'Andrew Martin & Michael Hausenblas'
+const imgSource = "https://images-na.ssl-images-amazon.com/images/I/41keUsJJxPL._SX379_BO1,204,203,200_.jpg"
+
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
+      <Book title = {title} author={author} imgSource={imgSource}/>
     </section>
   );
 }
 
-function Book() {
+function Book(props) {
+  const {title, author, imgSource} = props
+
   return <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src={imgSource}/>
+    <h2>{title}</h2>
+    <h4>{author}</h4>
   </article>;
-}
-
-function Image() {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/41keUsJJxPL._SX379_BO1,204,203,200_.jpg"/>
-}
-
-function Title() {
-  return <h2>Hacking Kubernetes: Threat-Driven Analysis and Defense</h2>
-}
-
-function Author() {
-  return <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Andrew Martin & Michael Hausenblas</h4>
 }
 
 const root = createRoot(document.getElementById("root"))
