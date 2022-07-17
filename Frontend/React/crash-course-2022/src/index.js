401751bbdcd84debacb645b52619f13cdf242e51
Pedro Arenas <pedroars99@icloud.com>
2022-07-17 12:43:35 +0200

    [React] Basic Booklist

--- Frontend/React/crash-course-2022/src/index.js
+++ Frontend/React/crash-course-2022/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
+  return (
+    <section className="bookList">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Book() {
+  return <article className="book">
+    <Image />
+    <Title />
+    <Author />
+  </article>;
+}
+
+function Image() {
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/41keUsJJxPL._SX379_BO1,204,203,200_.jpg"/>
+}
+
+function Title() {
+  return <h2>Hacking Kubernetes: Threat-Driven Analysis and Defense</h2>
+}
+
+function Author() {
+  return <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Andrew Martin & Michael Hausenblas</h4>
+}
+
+const root = createRoot(document.getElementById("root"))
+root.render(<BookList />);
