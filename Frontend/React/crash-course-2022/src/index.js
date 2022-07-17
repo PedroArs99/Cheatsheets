9e61fcc76b69acac0c21e8f48e3bbae3b64c7563
Pedro Arenas <pedroars99@icloud.com>
2022-07-17 15:45:03 +0200

    [React] Basic Event handling

--- Frontend/React/crash-course-2022/src/index.js
+++ Frontend/React/crash-course-2022/src/index.js
@@ -29,10 +29,15 @@ function BookList() {
 }
 
 function Book({title, author, img}) {
+  const clickHandler = (author) => alert(author)
+
   return <article className="book">
     <img src={img}/>
     <h2>{title}</h2>
     <h4>{author}</h4>
+    <button type="button" onClick={() => clickHandler(author)}>
+      Alert the author
+    </button>
   </article>;
 }
 
