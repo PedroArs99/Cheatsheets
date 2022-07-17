e9c8e61becac1b281d07700a2b02394b8cf025fd
Pedro Arenas <pedroars99@icloud.com>
2022-07-17 13:10:37 +0200

    [React] Destructure props

--- Frontend/React/crash-course-2022/src/index.js
+++ Frontend/React/crash-course-2022/src/index.js
@@ -4,23 +4,32 @@ import { createRoot } from "react-dom/client";
 // CSS
 import './index.css';
 
-const title = 'Hacking Kubernetes: Threat-Driven Analysis and Defense'
-const author = 'Andrew Martin & Michael Hausenblas'
-const imgSource = "https://images-na.ssl-images-amazon.com/images/I/41keUsJJxPL._SX379_BO1,204,203,200_.jpg"
+const hackingK8s = {
+  title: 'Hacking Kubernetes: Threat-Driven Analysis and Defense',
+  author: 'Andrew Martin & Michael Hausenblas',
+  img: "https://images-na.ssl-images-amazon.com/images/I/41keUsJJxPL._SX379_BO1,204,203,200_.jpg"
+}
+
+const cleanCode = {
+  title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+  author: 'Robert C. Martin',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg'
+}
 
 function BookList() {
   return (
     <section className="bookList">
-      <Book title = {title} author={author} imgSource={imgSource}/>
+      <Book book={hackingK8s}/>
+      <Book book={cleanCode}/>
     </section>
   );
 }
 
-function Book(props) {
-  const {title, author, imgSource} = props
+function Book({book}) {
+  const {title, author, img} = book
 
   return <article className="book">
-    <img src={imgSource}/>
+    <img src={img}/>
     <h2>{title}</h2>
     <h4>{author}</h4>
   </article>;
